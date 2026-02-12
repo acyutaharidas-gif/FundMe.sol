202dba17fc18f0cf8510894d8e1362b75914b92b
acyutaharidas-gif <acyutaharidas@gmail.com>
2026-02-12 17:15:53 +0530

    add withdraw function

--- FundMe.sol
+++ FundMe.sol
@@ -7,10 +7,16 @@ contract FundMe {
 
     using PriceConverter for uint256;
 
-    uint256 public minUsd = 1;
+    uint256 public minUsd = 1e18;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         require(msg.value.getConversionRate() >= minUsd, "didn't send enough ETH");
@@ -19,10 +25,32 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder  = funders[funderIndex];
             addressToAmountFunded[funder] = 0;   
         }
+
+        // reset the array
+        funders = new address[](0);
+        // withdraw funds
+
+        // transfer (deprecated)
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send (deprecated)
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be owner!");
+        _; //execute the rest of the code
+    }
+
 }
